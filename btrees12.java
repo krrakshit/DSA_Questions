1a5cecdbfcfc3e30ab8289222efae9e1527948f9
krrakshit <rkrkr365@gmail.com>
2024-09-03 12:20:38 +0530

    Create btrees12.java

---
+++ btrees12.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+import javax.swing.tree.TreeNode;
+public class btrees12 {
+    // vertical order traversal of a binary tree
+    public List<List<Integer>> verticaltraversal(TreeNode root){
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (node == null) {
+            return ans;
+        }
+        int col = 0;
+        Queue<Map.Entry<TreeNode,Integer>> queue = new ArrayDeque<>();
+        Map<Integer , ArrayList<Integer>> map = new HashMap();
+
+        queue.offer(new Abstractmap.SimpleEntry<>(node,col));
+        int min = 0;
+        int max = 0;
+        while (!queue.isEmpty()) {
+            
+            
+        }
+    }
+    
+}
