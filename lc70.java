4b1df499f41ad9bacd0a935aca86eac095291175
krrakshit <rkrkr365@gmail.com>
2024-10-26 23:41:29 +0530

    ..

---
+++ lc70.java
@@ -0,0 +1,16 @@
+public class lc70 {
+    public static int counttab(int n){
+        int dp[] = new int[n+1];
+        dp[0] =1;
+        for (int i = 1;i<=n;i++){
+            if (i==1){
+                dp[i] = dp[i-1];
+            }
+            else{
+                dp[i] = dp[i-1] +dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+    
+}
