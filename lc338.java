50deb17b37e4432219f9a4cbdb813a32b3713de2
krrakshit <rkrkr365@gmail.com>
2024-10-27 23:47:30 +0530

    Create lc338.java

---
+++ lc338.java
@@ -0,0 +1,11 @@
+public class lc338 {
+    public int[] countBits(int n) {
+        int []ans = new int[n+1];
+        for(int i = 0;i<=n;i++){
+            ans[i] = ans[i / 2] + (i % 2);
+        }
+        return ans;
+        
+    }
+    
+}
