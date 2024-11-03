e201be36670b61b677445a11433561e3c9453fe9
krrakshit <rkrkr365@gmail.com>
2024-11-03 23:52:30 +0530

    Create lc26.java

---
+++ lc26.java
@@ -0,0 +1,20 @@
+public class lc26 {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+          int uniqueCount = 0;
+          
+            
+            for (int currentNum : nums) {
+                
+                if (uniqueCount == 0 || currentNum != nums[uniqueCount - 1]) {
+                    
+                    nums[uniqueCount++] = currentNum;
+                }
+            }
+          
+            
+            return uniqueCount;  
+        }
+    }
+    
+}
